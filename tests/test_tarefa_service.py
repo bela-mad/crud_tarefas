b63dd1bd54bb276f18663c9bb1b141705eda59f4
bela-mad <isabelaargolo3110@gmail.com>
2026-05-23 22:46:44 -0300

    Adição de testes de listagem (get_all) e busca por id (get_by_id)

--- tests/test_tarefa_service.py
+++ tests/test_tarefa_service.py
@@ -18,6 +18,34 @@ class TestTarefaService(unittest.TestCase):
         self.assertEqual(tarefa["descricao"], "Revisar roteamento distribuído")
         self.assertEqual(tarefa["status"], "pendente")
 
+    def test_get_all_tarefas(self):
+        self.service.create({
+            "titulo": "Estudar Ciência de Dados",
+            "descricao": "Aprender os conceitos de Machine Learning",
+            "status": "concluída"
+        })
+
+        self.service.create({
+            "titulo": "Fazer atividade de Engenharia de Software",
+            "descricao": "Fazer relatório da atividade sobre TDD",
+            "status": "pendente"
+        })
+
+        tarefas = self.service.get_all()
+
+        self.assertEqual(len(tarefas), 2)
+
+    def test_get_tarefa_by_id(self):
+        tarefa_criada = self.service.create({
+            "titulo": "Ler sobre Git",
+            "descricao": "Revisar comandos básicos do Git",
+            "status": "pendente"
+        })
+
+        tarefa_encontrada = self.service.get_by_id(tarefa_criada["id"])
+
+        self.assertEqual(tarefa_encontrada["titulo"], "Ler sobre Git")
+
 
 if __name__ == "__main__":
     unittest.main()
