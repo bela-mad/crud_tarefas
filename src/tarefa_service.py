8ae76506a0709e7f0341298f69473e94fb64da2a
bela-mad <isabelaargolo3110@gmail.com>
2026-05-23 22:44:16 -0300

    Implementação do método de criação de tarefa

--- src/tarefa_service.py
+++ src/tarefa_service.py
@@ -1,2 +1,17 @@
 class TarefaService:
-    pass
+    def __init__(self):
+        self.tarefas = []
+        self.proximo_id = 1
+
+    def create(self, dados):
+        nova_tarefa = {
+            "id": self.proximo_id,
+            "titulo": dados["titulo"],
+            "descricao": dados["descricao"],
+            "status": dados["status"]
+        }
+
+        self.tarefas.append(nova_tarefa)
+        self.proximo_id += 1
+
+        return nova_tarefa
